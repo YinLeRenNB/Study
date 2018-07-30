d47d61f8787b2ad76dfdc73f6ebec2117207c111
sumi <1070844242@qq.com>
2018-07-30 16:01:31 +0800

    vue

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,15 +1,37 @@
 <template>
   <div id="app">
+    <div>   
+      <button @click="goBack()">后退</button>
+      <button @click="goForward()">前进</button>
+      <button @click="goHome()">返回首页</button>
+    </div>
+
     <router-link to="/index">首页</router-link>
     <router-link to="/list">列表页</router-link>
-    <router-view></router-view>
+    <router-link to="/url/hahah/20">Url传参</router-link>
+    <router-link to="/home">重定位</router-link>
+
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>    
+    </transition>
 
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    goBack(){
+      this.$router.go(-1);
+    },
+    goForward(){
+      this.$router.go(1);
+    },
+    goHome(){
+      this.$router.push('/');
+    }
+  }
 }
 </script>
 
@@ -22,4 +44,24 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.fade-enter{
+  opacity: 0;
+}
+.fade-enter-active{
+  transition: opacity 2s;
+}
+.fade-enter-to{
+  opacity: 1;
+}
+.fade-leave{
+  opacity: 1;  
+}
+.fade-leave-active{
+  transition: opacity 1s;
+}
+.fade-leave-to{
+  opacity: 0;  
+}
+
 </style>
